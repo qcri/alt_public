108a81962758542f1cf5118cbb8f1a908a60b2ff
Alaa Hamoudah <alaakhalil11@gmail.com>
2020-09-06 21:59:17 +0300

    Add unittest class to test_bibtex

--- test_bibtex.py
+++ test_bibtex.py
@@ -1,25 +1,28 @@
 import bibtexparser
+import unittest
 
-def test_tanbih():
-  with open('tanbih.bib') as bibtex_file:
-    bib_database = bibtexparser.load(bibtex_file)
+class TestBibtexFiles(unittest.TestCase):
+  def test_tanbih(self):
+    with open('tanbih.bib') as bibtex_file:
+      bib_database = bibtexparser.load(bibtex_file)
 
-def test_alt():
-  with open('alt.bib') as bibtex_file:
-    bib_database = bibtexparser.load(bibtex_file)
-    return not has_doubles(bib_database)
+  def test_alt(self):
+    with open('alt.bib') as bibtex_file:
+      bib_database = bibtexparser.load(bibtex_file)
+      self.assertFalse(self.has_doubles(bib_database))
 
-def has_doubles(elements):
-  """
-  check if two or more different objects have the same pdf-file 
-  """
-  elements = elements.get_entry_list()
-  final_list = []
-  has_duplicate = False; 
-  for elem in elements: 
-    if elem not in final_list: 
-        final_list.append(elem) 
-    else: 
-      print(elem) 
-      has_duplicate = True
-  return has_duplicate
+  def has_doubles(self, elements):
+    elements = elements.get_entry_list()
+    final_list = []
+    has_duplicate = False; 
+    for elem in elements: 
+      if elem not in final_list: 
+          final_list.append(elem) 
+      else: 
+        print(elem) 
+        has_duplicate = True  
+    return has_duplicate
+
+if __name__ == '__main__':
+  unittest.main()
+
